c9c1e8f633763bcce5449e1d214c0d8eacb235be
Claudio Díaz <claudio.diaz.morales@gmail.com>
2020-11-15 18:34:46 -0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,32 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+  <div>
+    <nav class="navbar navbar-dark bg-dark navbar-expand-lg fixed-top" id="mainNav">
+        <div class="container"><router-link class="navbar-brand" :to="{name:'Portada'}">V8</router-link><button data-toggle="collapse" data-target="#navbarResponsive" class="navbar-toggler" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><i class="fa fa-bars"></i></button>
+            <div
+                class="collapse navbar-collapse" id="navbarResponsive">
+                <div class="navbar-nav ml-auto">
+                      <router-link class="nav-link" :to="{name:'Portada'}">Portada</router-link>
+                      <router-link class="nav-link" :to="{name:'SobreMi'}">Sobre mi</router-link>
+                      <router-link class="nav-link" to="/contacto">Contacto</router-link>
+                      <router-link class="nav-link" to="/post/1">Blog</router-link>
+                </div>
+            </div>
+        </div>
+    </nav>
+    <router-view></router-view>
   </div>
+  
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+export default {
+  name: 'App'
 }
+</script>
 
-#nav {
-  padding: 30px;
-}
+<style scoped>
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+#menu-v8 a{
+  text-shadow: 1px 1px #000;
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,33 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'Portada',
+    component: () => import('../views/Portada.vue')
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/sobremi',
+    name: 'SobreMi',
+    component: () => import('../views/SobreMi.vue')
+  },
+  {
+    path: '/contacto',
+    name: 'Contacto',
+    component: () => import('../views/Contacto.vue')
+  },
+  {
+    path: '/post/:entrada',
+    name: 'Articulo',
+    component: () => import('../views/Articulo.vue')
+  },
+  {
+    path: '/*',
+    name: 'NotFound',
+    component: () => import('../views/NotFound.vue')
   }
 ]
 
